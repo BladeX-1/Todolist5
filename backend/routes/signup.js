2daabe7f5a6c1fe714cc66b8d264853b877eb0be
Abhishek <41145278+BladeX-1@users.noreply.github.com>
2025-07-27 03:46:29 +0000

    add signup route

---
+++ backend/routes/signup.js
@@ -0,0 +1,28 @@
+const { Router } = require("express");
+const { PrismaClient } = require("@prisma/client");
+
+router = Router();
+const prisma = new PrismaClient();
+
+router.post("/", async (req, res) => {
+    const username = req.body?.username;
+    const password = req.body?.password;
+
+    if (!username || !password) {
+        return res
+            .status(400)
+            .json({ msg: "username and password cannot be empty" });
+    }
+    const alreadyUser = await prisma.user.findFirst({ where: { username } });
+    if (alreadyUser) {
+        return res.status(400).json({ msg: "username already taken" });
+    }
+
+    const hashedpassword = await bcrypt.hash(password, 10);
+    const newUser = await prisma.user.create({
+        data: { username, password: hashedpassword },
+    });
+    return res.status(200).json({ msg: "user created successfully", username });
+});
+
+module.exports = router;
